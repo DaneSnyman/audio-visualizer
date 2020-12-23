ee45a44718781c2173f499c73c379c622d0b35de
Dane Snyman <danefreelance@gmail.com>
2020-12-23 04:19:55 +0200

    working

--- main.js
+++ main.js
@@ -3,19 +3,26 @@ const canvas = document.querySelector("canvas");
 canvas.height = innerHeight;
 canvas.width = innerWidth;
 const brush = canvas.getContext("2d");
+const radius = 150;
 
 // Audio
 const songs = [
   {
-    title: "Miss Jane - It's a fine day",
-    src: "assets/fineday.mp3",
-    enum: "fineday",
+    title: "Downlink - Ignition",
+    src: "assets/ignition.mp3",
+    enum: "ignition",
     playing: false,
   },
   {
     title: "Wamdue Project - King of My Castle",
     src: "assets/wamdue.mp3",
     enum: "wamdue",
+    playing: false,
+  },
+  {
+    title: "Miss Jane - It's a fine day",
+    src: "assets/fineday.mp3",
+    enum: "fineday",
     playing: false,
   },
 ];
@@ -37,20 +44,26 @@ const playSong = (name) => {
 };
 
 let data = new Uint8Array(analyser.frequencyBinCount);
+
 const draw = (data) => {
   data = [...data];
   const space = canvas.width / data.length;
 
   data.forEach((val, i) => {
+    if (i % 2 === 0) {
+      const x = radius * Math.cos(degrees_to_radians(i * 6));
+      const y = radius * Math.sin(degrees_to_radians(i * 6));
+      drawCircle(x + innerWidth / 2, y + innerHeight / 2, 1, val, i * 6, brush);
+    }
     // * Circles
-    if (i % 7 === 0) {
+    if (i % 5 === 0) {
       for (let index = 0; index < val; index++) {
         if (index % 7 === 0) {
           brush.beginPath();
           brush.arc(space * i, canvas.height - index, 2, 0, Math.PI * 2);
           if (index > 180) {
             brush.fillStyle = "red";
-          } else if (index > 80) {
+          } else if (index > 120) {
             brush.fillStyle = "yellow";
           } else {
             brush.fillStyle = "green";
@@ -60,23 +73,35 @@ const draw = (data) => {
         }
       }
     }
-
-    // * lines
-    // brush.beginPath();
-    // brush.moveTo(space * i, canvas.height);
-    // if (val > 200) {
-    //   brush.strokeStyle = "red";
-    // } else if (val > 100) {
-    //   brush.strokeStyle = "yellow";
-    // } else {
-    //   brush.strokeStyle = "green";
-    // }
-    // for (let index = 0; index < val; index++) {
-    //   brush.lineTo(space * i, canvas.height - index);
-    // }
-    // brush.stroke();
   });
 };
+
+function drawCircle(x, y, w, h, deg, ctx) {
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(degrees_to_radians(deg + 90));
+  if (h > 200) {
+    ctx.fillStyle = "red";
+    ctx.shadowColor = "red";
+    ctx.shadowBlur = 2;
+  } else if (h > 130) {
+    ctx.fillStyle = "yellow";
+    ctx.shadowColor = "yellow";
+    ctx.shadowBlur = 2;
+  } else {
+    ctx.fillStyle = "green";
+    ctx.shadowColor = "green";
+    ctx.shadowBlur = 2;
+  }
+  ctx.fillRect(-1 * (w / 2), -1 * (h / 2), w, h);
+  ctx.restore();
+}
+function degrees_to_radians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+function radians_to_degrees(radians) {
+  return (radians * 180) / Math.PI;
+}
 
 const animate = () => {
   if (canvas.width !== innerWidth || canvas.height !== innerHeight) {
@@ -84,13 +109,13 @@ const animate = () => {
     canvas.height = innerHeight;
   }
   requestAnimationFrame(animate);
-  brush.fillStyle = "rgba(20, 20, 20, 0.3)";
+  brush.fillStyle = "rgba(20, 20, 20, 0.5)";
   brush.fillRect(0, 0, canvas.width, canvas.height);
   analyser.getByteFrequencyData(data);
   draw(data);
 };
 
-const init = () => {
+const init = async () => {
   if (!audioElement.src) {
     audioElement.src = songs[0].src;
   }
@@ -104,20 +129,24 @@ audioElement.onplay = () => {
   audioContext.resume();
 };
 
-audioElement.onended = () => {
-  songs.forEach((song, i) => {
-    console.log("song", song.src);
-    console.log("audioEle", audioElement.src);
-    if (
-      song.src === audioElement.src.substr(audioElement.src.indexOf("/", 7) + 1)
-    ) {
-      const arrIndex = i + 1;
-      const next = songs[arrIndex];
-      playSong(next.enum);
-    } else {
-      console.log("start list again");
+const selectSong = () => {
+  songs.every((song, i) => {
+    const currentSrc = audioElement.src.substr(
+      audioElement.src.indexOf("/", 7) + 1
+    );
+    if (i >= songs.length - 1) {
       audioElement.src = songs[0].src;
       playSong(songs[0].enum);
+      return false;
+    } else if (song.src === currentSrc) {
+      const arrIndex = +i + 1;
+      const next = songs[arrIndex];
+      playSong(next.enum);
+      return false;
     }
+    return true;
   });
+};
+audioElement.onended = () => {
+  selectSong();
 };
